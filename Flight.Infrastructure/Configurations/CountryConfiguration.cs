e9c83d77957b880fee15573de3b4517a0efb4447
RANOELISON Dimbisoa Patrick <dimbisoapatrick@gmail.com>
2025-01-07 15:44:48 +0300

    change bilder rf name

--- Flight.Infrastructure/Configurations/CountryConfiguration.cs
+++ Flight.Infrastructure/Configurations/CountryConfiguration.cs
@@ -6,6 +6,6 @@ public class CountryConfiguration : IEntityTypeConfiguration<Country>
 {
     public void Configure(EntityTypeBuilder<Country> builder)
     {
-        modelBuilder.Entity<Country>().Navigation(e => e.City).AutoInclude();
+        builder.Entity<Country>().Navigation(e => e.City).AutoInclude();
     }
 }
